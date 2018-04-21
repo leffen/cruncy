2af420a81e143af94e4fb385fdaaa405e1027266
leffen <leffen@gmail.com>
2018-04-21 11:26:42 +0200

    Added cli tests for app options

--- cli_option_test.go
+++ cli_option_test.go
@@ -1,6 +1,11 @@
 package cruncy
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestOptionCli(t *testing.T) {
 	o := NewCliOption("test")
@@ -12,11 +17,42 @@ func TestOptionCli(t *testing.T) {
 	}
 }
 
-func TestBool(t *testing.T) {
-	o := NewCliOption("asn")
+func TestBoolEnv(t *testing.T) {
+	os.Setenv("SYNC_UP", "true")
 
+	o := NewCliOption("test")
 	o.MakeBool("sync_up", "u", "SYNC_UP", false, "Sync directory up to S3")
-	if o.GetBool("sync_up") {
-		t.Error("Expected flag to be false by default")
-	}
+	o.ReadConfig()
+
+	assert.Equal(t, true, o.GetBool("sync_up"))
+}
+func TestBoolCli(t *testing.T) {
+	os.Clearenv()
+	os.Args = []string{os.Args[0], "-u", "true"}
+
+	o := NewCliOption("test")
+	o.MakeBool("sync_up", "u", "SYNC_UP", false, "Sync directory up to S3")
+	o.ReadConfig()
+
+	assert.Equal(t, true, o.GetBool("sync_up"))
+
+}
+
+func TestIntEnv(t *testing.T) {
+	os.Setenv("SYNC_UP", "177")
+
+	o := NewCliOption("test")
+	o.MakeInt("sync_up", "u", "SYNC_UP", 1, "Sync directory up to S3")
+	o.ReadConfig()
+	assert.Equal(t, 177, o.GetInt("sync_up"))
+}
+
+func TestIntCli(t *testing.T) {
+	os.Clearenv()
+	os.Args = []string{os.Args[0], "-u", "66"}
+
+	o := NewCliOption("test")
+	o.MakeInt("sync_up", "u", "SYNC_UP", 99, "Sync directory up to S3")
+	o.ReadConfig()
+	assert.Equal(t, 66, o.GetInt("sync_up"))
 }
